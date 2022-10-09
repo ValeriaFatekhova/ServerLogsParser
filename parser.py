87acaf95c5a7a7921219935c414a6c77793c7033
Valeria <kuminova_v@mail.ru>
2022-10-10 02:56:54 +0300

    Parser start 1

--- parser.py
+++ parser.py
@@ -1,2 +1,22 @@
+import os
+
+
+def log_parser(log_file_path):
+    res = dict()
+    with open(log_file_path) as f:
+        f.readline()
+
+    return res
+
+
+def print_json(json):
+    print(json)
+
+
 path = input("Введите путь к логфайлу или каталогу с логфайлами: ")
-print(path)
+
+for file in os.listdir(path):
+    print(os.path.join(path, file))
+    print_json(log_parser(os.path.join(path, file)))
+
+
